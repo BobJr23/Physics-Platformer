dbcfbb7abdd9189621b883c48e47db760a36f77a
Andrew Ma <ama25@belmontschools.net>
2024-07-17 18:08:30 -0400

    doors and buttons, more pymunk spaces and drawing

--- game.py
+++ game.py
@@ -4,9 +4,13 @@ import pymunk.pygame_util
 import math, sys, random
 
 pygame.init()
+
+# Pygame window setup
 width, height = 1600, 1000
 window = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Fireboy and Watergirl!")
+pygame.display.set_caption("Physics Platformer!")
+
+# Colors
 white = (255, 255, 255)
 green = (50, 168, 82)
 black = (0, 0, 0)
@@ -14,8 +18,9 @@ red = (200, 0, 0)
 grey = (150, 150, 150)
 blue = (66, 185, 189)
 brown = (79, 44, 41)
+
+# Pygame variables
 FONT = pygame.font.Font(None, 40)
-#
 clock = pygame.time.Clock()
 FPS = 60
 dt = 1 / FPS
@@ -33,11 +38,59 @@ class Player:
     def die(self):
         self.body.position = self.starting
 
+class Button:
+    def __init__(self, door, body, shape) -> None:
+        self.door = door
+        self.body = body
+        self.shape = shape
+
+
+class Door:
+    def __init__(self, body: pymunk.Body, shape, change_x, change_y, stop=None) -> None:
+        self.button = None
+        self.body = body
+        self.shape = shape
+        self.start = body.position
+        self.change_x = change_x
+        self.change_y = change_y
+        self.moving = False
+        self.stop = stop
+
+    def button_pressed(
+            self,
+    ):
+        if not self.body.position == self.stop:
+            self.body.position += (self.change_x, self.change_y)
+            # self.body.velocity = (self.change_x, self.change_y)
+        self.moving = True
+
+    def home_state(self):
+        self.body.position -= (self.change_x, self.change_y)
+
+def draw(space, window, draw_options, time):
+    window.fill((105, 78, 76))
+    space.debug_draw(draw_options)
+    window.blit(
+        FONT.render(
+            str(int(time)),
+            False,
+            (0, 0, 0),
+        ),
+        (width / 2, 30),
+    )
+    pygame.display.update()
+
+time = 0
 while run:
+    space = pymunk.Space()
+    space.gravity = (0, 980)
+    draw_options = pymunk.pygame_util.DrawOptions(window)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    window.fill(white)
-    pygame.display.update()
+    draw(space, window, draw_options, time)
+    space.step(dt)
     clock.tick(FPS)
+    time += 1 / 60
+pygame.quit()
